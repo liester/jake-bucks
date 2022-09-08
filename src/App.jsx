3dab02a8d4514445bf94c4ab29dbb2434f2ff6e4
Levi Liester <levi.liester@gmail.com>
2022-09-07 22:47:37 -0500

    Remove fluff

--- src/App.jsx
+++ src/App.jsx
@@ -39,26 +39,9 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        {Object.keys(users).map((user) => (
-          <div>{`${user} - ${JSON.stringify(users[user])}`}}</div>
-        ))}
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {Object.keys(users).map((user) => (
+        <div>{`${user} - ${JSON.stringify(users[user])}`}}</div>
+      ))}
     </div>
   );
 }
