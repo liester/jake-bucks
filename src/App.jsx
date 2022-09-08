17984bc85fd6ab6f4374b209362fcc89ba2ad7a5
Levi Liester <levi.liester@gmail.com>
2022-09-07 21:22:00 -0500

    all the bucks

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCGNf9oWiwbzD0MZT6mFvpU_AdpEVgfDCk",
+    authDomain: "jake-bucks.firebaseapp.com",
+    projectId: "jake-bucks",
+    storageBucket: "jake-bucks.appspot.com",
+    messagingSenderId: "865351729238",
+    appId: "1:865351729238:web:5ad98c062993221249fbf7",
+    measurementId: "G-3XXMBXKC00"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
