109b354ff9cfd74a7cc910d8bcce759899f14ca4
Levi Liester <levi.liester@gmail.com>
2022-09-07 22:41:28 -0500

    gimmie those bucks!

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, onValue } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCGNf9oWiwbzD0MZT6mFvpU_AdpEVgfDCk",
-    authDomain: "jake-bucks.firebaseapp.com",
-    projectId: "jake-bucks",
-    storageBucket: "jake-bucks.appspot.com",
-    messagingSenderId: "865351729238",
-    appId: "1:865351729238:web:5ad98c062993221249fbf7",
-    measurementId: "G-3XXMBXKC00"
+  apiKey: "AIzaSyCGNf9oWiwbzD0MZT6mFvpU_AdpEVgfDCk",
+  authDomain: "jake-bucks.firebaseapp.com",
+  projectId: "jake-bucks",
+  storageBucket: "jake-bucks.appspot.com",
+  messagingSenderId: "865351729238",
+  appId: "1:865351729238:web:5ad98c062993221249fbf7",
+  measurementId: "G-3XXMBXKC00",
+  databaseURL: "https://jake-bucks-default-rtdb.firebaseio.com",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const database = getDatabase(app);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const starCountRef = ref(database, "users/");
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log(JSON.stringify(data));
+      setUsers(data);
+    });
+  }, []);
 
   return (
     <div className="App">
@@ -38,9 +49,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        {Object.keys(users).map((user) => (
+          <div>{`${user} - ${JSON.stringify(users[user])}`}}</div>
+        ))}
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -49,7 +60,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
